f9f0b013fef695848049754a6892c22edca15911
Jim Hill <jim@dammitjim.co.uk>
2017-10-10 11:53:57 +0100

    Formaterinos

--- healthysnake/checkers/storage.py
+++ healthysnake/checkers/storage.py
@@ -4,5 +4,8 @@ import psutil
 def check_remaining_capacity(mountpoint, failure_threshold=90.0):
     def callback():
         disk_usage = psutil.disk_usage(mountpoint)
-        return disk_usage.percent < failure_threshold, 'mount point "{0}" reached {1}% capacity'.format(mountpoint, disk_usage.percent)
+        return disk_usage.percent < failure_threshold, 'mount point "{0}" reached {1}% capacity'.format(
+            mountpoint,
+            disk_usage.percent,
+        )
     return callback
