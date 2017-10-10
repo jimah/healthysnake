95f4ebc5a43847dbe8497133baa61455ad1c5abd
Jim Hill <jim@dammitjim.co.uk>
2017-10-10 11:45:21 +0100

    Adding checker requirements to requirements file

--- healthysnake/checkers/system.py
+++ healthysnake/checkers/system.py
@@ -2,6 +2,7 @@ import psutil
 
 
 def check_memory_utilisation(failure_threshold=80.0):
+    """Check that the memory utilisation of the system falls under the failure threshold."""
     def callback():
         vmem = psutil.virtual_memory()
         return vmem.percent < failure_threshold, 'currently sitting at {0}% memory consumption'.format(vmem.percent)
