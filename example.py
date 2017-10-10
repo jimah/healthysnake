9f7d58d8d559d9ca7566054f099fdf6c140a2084
Jim Hill <jim@dammitjim.co.uk>
2017-10-10 11:03:51 +0100

    Updating example failure thresholds

--- example.py
+++ example.py
@@ -18,9 +18,9 @@ def custom_dependency_check():
 
 # hc.add_dependency('soft_failure', check_soft_failure, level=levels.SOFT)
 hc.add_dependency('REDIS', redis.check_redis_connection('127.0.0.1'))
-hc.add_dependency('DISK SPACE', storage.check_remaining_capacity('/', failure_threshold=15),
+hc.add_dependency('DISK SPACE', storage.check_remaining_capacity('/', failure_threshold=90.0),
                   level=levels.SOFT)
-hc.add_dependency('RAM', system.check_memory_utilisation())
+hc.add_dependency('RAM', system.check_memory_utilisation(failure_threshold=75.0))
 
 
 print(hc.status())
