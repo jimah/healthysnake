01c0a062c5085886942ba7ed7f24bbacf2c6b4fb
Dan Stringer <dan.stringer1983@gmail.com>
2017-10-13 15:39:58 +0100

    Add test docstrings

--- healthysnake/tests/test_dependency.py
+++ healthysnake/tests/test_dependency.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from freezegun import freeze_time
 
 from healthysnake import levels
 from healthysnake.dependency import Dependency
@@ -9,7 +10,13 @@ def success_check():
 
 
 class TestDependencyInitialisation(object):
+    """
+    Tests initialization of the dependency class
+    """
     def test_defaults(self):
+        """
+        The dependency should be initialized with appropriate default values
+        """
         srv = Dependency('sherlock', success_check)
         assert srv.name == 'sherlock'
         assert srv._check == success_check
@@ -18,23 +25,36 @@ class TestDependencyInitialisation(object):
         assert srv._interval == timedelta(seconds=Dependency.DEFAULT_INTERVAL)
 
     def test_str(self):
+        """
+        The string representation of the dependency should return the dependency name, state and failure level
+        """
         srv = Dependency('sherlock', success_check)
         stringified = str(srv)
         assert stringified == 'sherlock [state=healthy, level=2]'
 
     def test_state_str(self):
+        """
+        The method should return the appropriate value depending on the value of the _healthy attribute
+        """
         srv = Dependency('sherlock', success_check)
         assert srv._state_str() == Dependency.STATE_HEALTHY
         srv._healthy = False
         assert srv._state_str() == Dependency.STATE_UNHEALTHY
 
+    @freeze_time('2017-1-1 12:00:00')
     def test_due(self):
+        """
+        The method should return False if checked within last <interval> seconds, otherwise True
+        """
         srv = Dependency('sherlock', success_check)
         assert srv.due() is False
         srv.last_updated = srv.last_updated - timedelta(seconds=60)
         assert srv.due() is True
 
     def test_healthy(self):
+        """
+        The method should identify whether the dependency is healthy or not
+        """
         srv = Dependency('sherlock', success_check)
         srv._healthy = False
         assert srv.healthy() == (False, '')
